d2972afa07b9e46ed74a20fc2ff93cae9286b12c
valdrin18 <drinsmakaj@hotmail.com>
2023-11-19 02:01:33 +0100

    added new comonent structure for testing

--- restaurant/src/pages/_app.js
+++ restaurant/src/pages/_app.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import '../styles/globals.css';
 
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
---
+++ restaurant/src/pages/improvedStructure.js
@@ -0,0 +1,62 @@
+// Menu.js -- render this at index.js 
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div className="bg-white p-6 border border-gray-300 rounded">
+      <MenuList />
+      <div className="my-4 border-t border-gray-300"></div> {/* Divider */}
+      <MenuItemList />
+      <div className="my-4 border-t border-gray-300"></div> {/* Divider */}
+      <OrderSummary />
+    </div>
+  );
+};
+
+export default Menu;
+
+// MenuList.js
+const MenuList = () => {
+  const dummyItems = Array.from({ length: 28 }, (_, i) => `Item ${i + 1}`);
+  return (
+    <div className="p-4">
+      <input type="search" placeholder="Search menu..." className="mb-4 p-2 border border-gray-300 rounded w-full" />
+      <div className="grid grid-cols-5 gap-4 overflow-y-auto" style={{ maxHeight: '27vh' }}>
+        {dummyItems.map((item) => (
+          <div key={item} className="p-2 bg-blue-100 text-center rounded">{item}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// MenuItemList.js
+const MenuItemList = () => {
+  const dummyTypes = Array.from({ length: 28 }, (_, i) => `Type ${i + 1}`);
+  return (
+    <div className="p-4">
+      <div className="font-bold mb-2">Coffee Types:</div>
+      <div className="grid grid-cols-5 gap-4 overflow-y-auto" style={{ maxHeight: '27vh' }}>
+        {dummyTypes.map((type) => (
+          <div key={type} className="p-2 bg-green-100 rounded">{type}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// OrderSummary.js
+const OrderSummary = () => {
+  const dummySummary = Array.from({ length: 8 }, (_, i) => `Order ${i + 1}`);
+  return (
+    <div className="p-4">
+      <div className="font-bold mb-2">Order Summary:</div>
+      <div className="flex flex-col gap-2 overflow-y-auto rounded border border-gray-300 p-2" style={{ maxHeight: '24vh' }}>
+        {dummySummary.map((order) => (
+          <div key={order} className="p-2 bg-yellow-100 text-center rounded">{order}</div>
+        ))}
+      </div>
+      <div className="mt-4 font-bold">Total price:</div>
+    </div>
+  );
+};
