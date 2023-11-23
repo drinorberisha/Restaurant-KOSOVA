efb408d726ad9ea8deb9804380820c4a585aaf27
valdrin18 <drinsmakaj@hotmail.com>
2023-11-23 17:22:16 +0100

    tt

--- restaurant/src/pages/magazina/index.js
+++ restaurant/src/pages/magazina/index.js
@@ -1,6 +1,6 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import styles from './MagazinaPage.module.css';
+import styles from "./MagazinaPage.module.css";
 import Nav from "@/components/Navigation/nav";
 import InventoryTable from "@/components/InventoryTable/InventoryTable";
 import UserManagementTable from "@/components/UserManagementTable/UserManagementTable";
@@ -8,56 +8,50 @@ import UserManagementTable from "@/components/UserManagementTable/UserManagement
 const Magazina = () => {
   const router = useRouter();
   const [userRole, setUserRole] = useState(null);
-
-  const [currentView, setCurrentView] = useState('products'); 
+  const [currentView, setCurrentView] = useState("products");
 
   const handleUserManagementClick = () => {
-    setCurrentView('users');
+    setCurrentView("users");
   };
 
   const handleProductManagementClick = () => {
-    setCurrentView('products');
+    setCurrentView("products");
   };
 
-
   useEffect(() => {
-    const role = localStorage.getItem('userRole');
+    const role = localStorage.getItem("userRole");
     setUserRole(role);
-    // Redirect if not authorized
-    if (role !== 'manager' && role !== 'admin') {
-      router.push('/'); // Redirect to a different page
+    if (role !== "manager" && role !== "admin") {
+      router.push("/");
     }
   }, [router]);
 
   return (
     <>
-        <Nav />
-        <div className={styles.background + " flex-grow"}>
-        <div className="flex flex-col h-screen">
-        <div className={`${styles.matrix} relative w-full h-full my-[5%] mx-[5%]`}>
-
-
-    {userRole === 'admin' && (
-      <div>
-        <button onClick={handleProductManagementClick} className="bg-blue-500 text-white p-2 mr-3 rounded">
-          Manage Products
-        </button>
-        <button onClick={handleUserManagementClick} className="bg-blue-500 text-white p-2 mr-3 rounded">
-          Manage Users
-        </button>
-      </div>
-    )}
-
-{currentView === 'products' ? (  
-            <InventoryTable/>
-    
-   ):(
-      <UserManagementTable/>
-      
-    )}
+      <Nav />
+      <div className={styles.background + " flex justify-center items-center h-screen"}>
+        <div className="flex flex-col">
+          {userRole === "admin" && (
+            <div className="mb-5"> 
+              <button
+                onClick={handleProductManagementClick}
+                className="bg-blue-500 text-white p-2 mr-3 rounded"
+              >
+                Manage Products
+              </button>
+              <button
+                onClick={handleUserManagementClick}
+                className="bg-blue-500 text-white p-2 rounded"
+              >
+                Manage Users
+              </button>
             </div>
+          )}
+          <div className="bg-white p-8 rounded"> 
+            {currentView === "products" ? <InventoryTable /> : <UserManagementTable />}
+          </div>
+        </div>
       </div>
-   </div>
     </>
   );
 };
